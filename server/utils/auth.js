ef75065d0ad6ac8bf3152eaeca69b7cc1fb8e74b
Collins418 <106499144+Collins418@users.noreply.github.com>
2022-12-04 13:56:10 -0600

    fix utils

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,18 +1,19 @@
-const jwt = require('jsonwebtoken');
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
 
 // set token secret and expiration date
-const secret = 'mysecretsshhhhh';
-const expiration = '2h';
+const secret = process.env.SECRET;
+const expiration = "2h";
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+  authMiddleware: function ({ req }) {
     // allows token to be sent via  req.query or headers
     let token = req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
+      token = token.split(" ").pop().trim();
     }
 
     if (!token) {
@@ -25,10 +26,8 @@ module.exports = {
       req.user = data;
     } catch {
       console.log("Invalid token");
-      console.log(token);
     }
 
-    // send to next endpoint
     return req;
   },
   signToken: function ({ username, email, _id }) {
