1548789645e610aaea8908a80335af5a759a6a9d
Collins418 <106499144+Collins418@users.noreply.github.com>
2022-12-04 13:46:24 -0600

    add three lines to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,27 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import SearchBooks from './pages/SearchBooks';
-import SavedBooks from './pages/SavedBooks';
-import Navbar from './components/Navbar';
-
+import React from "react";
 import {
+  ApolloProvider,
   ApolloClient,
   InMemoryCache,
-  ApolloProvider,
   createHttpLink,
 } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SearchBooks from "./pages/SearchBooks";
+import SavedBooks from "./pages/SavedBooks";
+import Navbar from "./components/Navbar";
 
-const httpLink = createHttpLink({ uri: "/graphql" });
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem("id_token");
   return {
-    headers: { ...headers, authorization: token ? `Bearer ${token}` : "" },
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
   };
 });
 
